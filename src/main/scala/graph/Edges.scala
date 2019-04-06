8b0b3fca4a2566c09d00d85aa269e100465b6d9e
youngseo <ys550499@kaist.ac.kr>
2019-04-07 08:55:44 +0900

    Add equal case in Edge class

--- src/main/scala/graph/Edges.scala
+++ src/main/scala/graph/Edges.scala
@@ -2,6 +2,7 @@ package graph
 
 class Edges(edges: Map[Set[City], Double]) {
   def apply(city1: City, city2: City): Double = {
-    edges(Set(city1, city2))
+    if (city1.equals(city2)) 0
+    else edges(Set(city1, city2))
   }
 }
