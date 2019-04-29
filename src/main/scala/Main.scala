e2a798c011f07aa64b1e1c2f930880bc8059c931
youngseo <ys550499@kaist.ac.kr>
2019-04-29 21:45:52 +0900

    new Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,8 +6,10 @@ import util.{FileProcessor, Util}
 object Main {
   def main(args: Array[String]): Unit = {
     // input city with location of city using x, y coordination
-    val cities: List[City] = FileProcessor.fileReader("city.txt")
-    val optTour: List[String] = FileProcessor.optReader("opt.txt")
+    val cities: List[City] = FileProcessor.fileReader("att48.txt")
+    val optTour: List[String] = FileProcessor.optReader("att48.opt.txt")
+//    val cities: List[City] = FileProcessor.fileReader("pr2392.txt")
+//    val optTour: List[String] = FileProcessor.optReader("pr2392.opt.txt")
     val cityEdges = Util.getEdge(cities)
 
     val optLength = Util.traverse(cityEdges, cities, optTour)
